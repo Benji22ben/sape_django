4489d72dad49cbbfe40e448f610043bf1c9d74f7
Benjamin MARQUES BALULA <marques.benjaminb@gmail.com>
2024-03-20 00:59:51 +0100

    feat: added types related to clothes

--- clothes/models.py
+++ clothes/models.py
@@ -1,3 +1,95 @@
 from django.db import models
+from enum import Enum
+from django.contrib.postgres.fields import ArrayField
+
 
 # Create your models here.
+class Season(Enum):
+    WINTER = "winter"
+    SUMMER = "summer"
+    SPRING = "spring"
+    FALL = "fall"
+    ALL = "all"
+
+    @classmethod
+    def choices(cls):
+        return [(key.value, key.name) for key in cls]
+
+    @classmethod
+    def get_season(cls, temperature):
+        if temperature < 4:
+            return cls.WINTER.value
+        elif temperature >= 25:
+            return cls.SUMMER.value
+        elif temperature > 15 and temperature < 25:
+            return cls.SPRING.value
+        elif temperature >= 4 and temperature < 15:
+            return cls.FALL.value
+        else:
+            return cls.ALL.value
+
+
+class ClothesType(models.Model):
+    name = models.CharField(max_length=100)
+    season = models.CharField(
+        max_length=1, choices=Season.choices(), default=Season.ALL.value
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Pattern(models.Model):
+    name = models.CharField(max_length=100)
+    image = models.ImageField(upload_to="patterns")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Material(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    why_its_bad = models.TextField()
+    why_its_good = models.TextField()
+    score = models.IntegerField(max=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Brand(models.Model):
+    name = models.CharField(max_length=100)
+    reputation = models.IntegerField(max=100)
+    ecoreputation = models.IntegerField(max=100)
+    description = models.TextField()
+    why_its_bad = models.TextField()
+    why_its_good = models.TextField()
+    material_used = models.TextField()
+    fabrication_location = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    material = models.ManyToManyField(Material)
+
+    def __str__(self):
+        return self.name
+
+
+class Clothe(models.Model):
+    name = models.CharField(max_length=100)
+    image = models.ImageField(upload_to="clothes")
+    type = models.ForeignKey(ClothesType, on_delete=models.CASCADE)
+    pattern = models.ForeignKey(Pattern, on_delete=models.CASCADE)
+    color = ArrayField(models.CharField(max_length=6), default=list)
+    brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
