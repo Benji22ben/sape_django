7ed4deb788962e14287671315de50dccb9beb6ab
Benjamin MARQUES BALULA <marques.benjaminb@gmail.com>
2024-03-20 00:13:13 +0100

    Initial Commit

---
+++ account/serializers.py
@@ -0,0 +1,19 @@
+from rest_framework import serializers
+from .models import UserData
+
+
+class UserSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = UserData
+        fields = ["id", "email", "firstname", "lastname", "password"]
+
+    def create(self, validated_data):
+        user = UserData.objects.create(
+            email=validated_data["email"],
+            firstname=validated_data["firstname"],
+            lastname=validated_data["lastname"],
+        )
+        user.set_password(validated_data["password"])
+        user.save()
+        return user
