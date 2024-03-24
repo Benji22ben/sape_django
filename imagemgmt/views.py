2feadc422dc80c3c2f635cecc1520e77eaaef3e2
Benjamin MARQUES BALULA (VPS) <marques.benjaminb@gmail.com>
2024-03-24 15:12:17 +0000

    fix: need cuda

--- imagemgmt/views.py
+++ imagemgmt/views.py
@@ -3,14 +3,17 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework.response import Response
 from rembg import remove
-
+from rest_framework.views import APIView
+import PIL.Image
 
 # Create your views here.
 class RemoveBGImage(APIView):
     permission_classes = [IsAuthenticated]
     authentication_classes = [JWTAuthentication]
 
+    # NEED CUDA NOT WORKING
     def post(self, request, format=None):
         image = request.data["image"]
-        image = remove(image)
+        image = PIL.Image.open(image)
+        # image = remove(image)
         return Response({"image": image})
